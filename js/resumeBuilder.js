2ea7801077aa9c4ee941e6a16bb1024f78abe689
jsherman <jacob@jbsherman.com>
2016-11-03 12:30:10 -0400

    continuing dynamic dom creation from json objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,18 +2,6 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-// var bio = {
-//     "bio" : [
-//     {
-//         "name" : "Jacob Sherman",
-//         "role" : "Full Stack Developer",
-//         "contact" : "jacob@jbsherman.com",
-//         "welcome" : "Let me handle the matrix and free your mind to do other stuff!",
-//         "skills" : [JavaScript, PHP, Linux]
-//     }
-//         ]
-// }
-
 var bio = {
     "name" : "Jacob Sherman",
     "role" : "Full Stack Developer",
@@ -78,31 +66,76 @@ var education = {
 var work = {
     "jobs" : [
         {
-        "employer" :  ,
-        "title"  :   ,
-        "location"  :  ,
-        "dates"  :
+            "employer" :  "Pena & Kahn PLLC",
+            "title"  :   "Managing Attorney",
+            "location"  :  "New York",
+            "dates"  :  "2010 - 2012",
+            "description"  : "Managed a 10 person law office, handled heavy caseload, paralegal training and attorney supervision"
+        },
+        {
+            "employer" : "ScriptEd",
+            "title"    : "Web Programming Instructor",
+            "location" : "New York",
+            "dates"    : "2014 - 2015",
+            "description" : "Taught basic web programming skills to underprivileged New York City high school students."
         }
     ]
 }
+
+for (job in work.jobs){
+    $("#workExperience").append(HTMLworkStart);
+    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+    var formattedEmployerTitle = formattedEmployer + formattedTitle;
+    $(".work-entry:last").append(formattedEmployerTitle);
+
+    var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+    $(".work-entry:last").append(formattedWorkDates);
+}
+
 
 var projects = {
     "projects" : [
         {
-            "title"  :  ,
-            "dates"  :  ,
-            "description"  :  ,
-            "imageURL"  :
+            "title"  :  "Interactive Google Map",
+            "dates"  :  "2014",
+            "description"  :  "Created an interactive game using Google Maps, JavaScript and PHP/MySQL as my final project for Harvard’s Edx Introduction to Computer Science course",
+            "imageURL"  : "https://github.com/NYCJacob/pset8-interactive-map"
+        },
+        {
+            "title" : "Mariatemildagonzalezesq.com and nyrsccasolo.com",
+            "dates"  : "2013",
+            "description"  : "Created fully responsive websites using Bootstrap and custom JavaScript based on designer’s mockup.",
+            "imageURL"   : "http://Mariatemildagonzalezesq.com and http://nyrsccasolo.com"
+        },
+        {
+            "title"  :  "webesq.net",
+            "dates"  : "2013",
+            "description"  : "Developed website that produces legal documents for an uncontested divorce in New York City.  " +
+                    "Implemented client-side and server side verification using Javascript and PHP/MySQL code to ensure the user files the " +
+                    "correct documents and produces PDF files ready for court submission.",
+            "imageURL"  : "http://www.webesq.net"
+        },
+        {
+            "title"  : "ethoshopper.com",
+            "dates"  :  "2009 - 2010",
+            "description"  :  "Developed OSCommerce/PHP based website selling artisan ceramics from South America. " +
+                            "The website is no longer available.",
+            "imageURL"  : "https://web.archive.org/web/20110817085349/http://www.ethoshopper.com/"
         }
     ]
 }
 
 
-$('#header').prepend(bio);
+var  FormattedheaderName = HTMLheaderName.replace('%data%', bio.name);
+$("#header").prepend(FormattedheaderName);
 
-
-var formattedName = HTMLheaderName.replace('%data%', myName);
-var formattedRole = HTMLheaderRole.replace('%data%', myRole);
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
+// check if skill exist then append
+if (bio.skills.length !== 0){
+    $("#header").append(HTMLskillsStart);
+    var skillsLength = bio.skills.length;
+    for (var i in bio.skills) {
+        var skillFormatted =  HTMLskills.replace('%data%', bio.skills[i])
+        $("#skills").append(skillFormatted);
+    }
+}

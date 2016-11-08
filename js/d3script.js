2cdae6eabbc3a84cbaab5cf4a0a30714132a40dd
jsherman <jacob@jbsherman.com>
2016-11-07 22:03:14 -0500

    feat: started d3 implementation; style: cleaned up scipts, removed script tag in html

---
+++ js/d3script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by jsherman on 11/7/16.
+ */
+var data = [4, 8, 15, 16, 23, 42];
+
+d3.select("#chart")
+    .selectAll("div")
+    .data(data)
+    .enter().append("div")
+    .style("width", function(d) { return d * 10 + "px"; })
+    .text(function(d) { return d; });
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -314,13 +314,34 @@ function addProject(project) {
 }
 
 
-
-
 $('#mapDiv').append(googleMap);
 
-$(document).click(function(loc) {
-    // your code goes here
-    logClicks(loc.pageX, loc.pageY);
-});
+// $(document).click(function(loc) {
+//     // your code goes here
+//     logClicks(loc.pageX, loc.pageY);
+// });
 
+
+// this code remove empty elements
+if(document.getElementsByClassName('flex-item').length === 0) {
+    document.getElementById('topContacts').style.display = 'none';
+}
+if(document.getElementsByTagName('h1').length === 0) {
+    document.getElementById('header').style.display = 'none';
+}
+if(document.getElementsByClassName('work-entry').length === 0) {
+    document.getElementById('workExperience').style.display = 'none';
+}
+if(document.getElementsByClassName('project-entry').length === 0) {
+    document.getElementById('projects').style.display = 'none';
+}
+if(document.getElementsByClassName('education-entry').length === 0) {
+    document.getElementById('education').style.display = 'none';
+}
+if(document.getElementsByClassName('flex-item').length === 0) {
+    document.getElementById('lets-connect').style.display = 'none';
+}
+if(document.getElementById('map') === null) {
+    document.getElementById('mapDiv').style.display = 'none';
+}
 
